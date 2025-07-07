690d3ab3d1f7cfabaaffa3c486dfbf302b067637
KijinoParagon <andrew.honey@southeasttech.edu>
2025-07-06 22:45:08 -0500

    production test

--- src/components/Assistant.tsx
+++
@@ -1,7 +0,0 @@
-import { MouseEventHandler } from "react";
-
-export function Assistant({onClick}:{onClick:MouseEventHandler}){
-    return <div onClick={onClick} className="assistant">
-        <p>Assist me!</p>
-    </div>
-}
